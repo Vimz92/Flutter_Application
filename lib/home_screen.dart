030f37ea8a2b7237b58d26a9a0d83b65e4b2bd69
vimal vinayraj <vimalvinayraj@gmail.com>
2023-06-19 21:20:25 +0400

    fixed form validation

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,52 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:master_flutter_application/login_screen.dart';
 
-class ScreenHome extends StatelessWidget {
+class ScreenHome extends StatefulWidget {
   const ScreenHome({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final _usernameController = TextEditingController();
-    final _passwordController = TextEditingController();
+  State<ScreenHome> createState() => _ScreenHomeState();
+}
 
-    if (_usernameController == _passwordController) {
-      print('username and password is valid');
-    } else {
-      print('username and password is not valid');
+class _ScreenHomeState extends State<ScreenHome> {
+  String name = '';
+  bool changeButton = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  moveToHome(BuildContext context) async {
+    if (_formKey.currentState?.validate() == true) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
+        return ScreenLogin();
+      }));
+      setState(() {
+        changeButton = false;
+      });
     }
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       // appBar: AppBar(),
       body: SafeArea(
-        child: Column(
-          children: [
-            Image.asset('assets/images/mobile_images.jpg'),
-            SizedBox(height: 10),
-            Text(
-              'Welcome to CodeBlasters',
-              style: TextStyle(
-                  color: Colors.red, fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Username'),
-            ),
-            TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Password')),
-            SizedBox(height: 10),
-            ElevatedButton.icon(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
-                    return ScreenLogin();
-                  }));
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/images/mobile_images.jpg',
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Welcome $name',
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value?.isEmpty == true) {
+                    return 'username cannot be empty';
+                  }
+                  return null;
                 },
-                icon: Icon(Icons.check),
-                label: Text('login'))
-          ],
+                onChanged: (value) {
+                  setState(() {
+                    name = value;
+                  });
+                },
+                // controller: _usernameController,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), hintText: 'Username'),
+              ),
+              TextFormField(
+                  validator: (value) {
+                    if (value?.isEmpty == true) {
+                      return 'password changed';
+                    } else if (value!.length < 6) {
+                      return 'password length should be atleast 6';
+                    }
+                    return null;
+                  },
+                  // controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Password')),
+              const SizedBox(height: 10),
+              Material(
+                child: InkWell(
+                  onTap: () {
+                    moveToHome(context);
+                  },
+                  child: AnimatedContainer(
+                    duration: Duration(seconds: 1),
+                    width: changeButton ? 50 : 150,
+                    height: 50,
+                    alignment: Alignment.center,
+                    child: changeButton
+                        ? Icon(Icons.done, color: Colors.white)
+                        : Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                    decoration: BoxDecoration(
+                      // shape: changeButton ? BoxShape.circle : BoxShape.rectangle,
+                      borderRadius:
+                          BorderRadius.circular(changeButton ? 50 : 8),
+                      color: Colors.green,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
